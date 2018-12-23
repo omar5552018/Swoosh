0abcb6869283d2aa1ad30c04c4c34b1ca21174c7
omar5552018 <omar5552016@hotmail.com>
2018-12-23 23:15:44 +0300

    add SecondPage in single branch

--- app/src/main/java/com/example/omaralsaedi/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/omaralsaedi/swoosh/LeagueActivity.kt
@@ -12,9 +12,29 @@ class LeagueActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
 
-//        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.beginnerballerbg)
-//        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
-//        rounded.cornerRadius = 200f
-//        check.setImageDrawable(rounded)
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.old)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 33f
+        oldImg.setImageDrawable(rounded)
+
+        val bitmap1 = BitmapFactory.decodeResource(resources, R.drawable.man)
+        val rounded1 = RoundedBitmapDrawableFactory.create(resources, bitmap1)
+        rounded1.cornerRadius = 33f
+        menImg.setImageDrawable(rounded1)
+
+
+        val bitmap2 = BitmapFactory.decodeResource(resources, R.drawable.girls)
+        val rounded2 = RoundedBitmapDrawableFactory.create(resources, bitmap2)
+        rounded2.cornerRadius = 33f
+        girlsImg.setImageDrawable(rounded2)
+
+
+        val bitmap3 = BitmapFactory.decodeResource(resources, R.drawable.kids)
+        val rounded3 = RoundedBitmapDrawableFactory.create(resources, bitmap3)
+        rounded3.cornerRadius = 33f
+        kidImg.setImageDrawable(rounded3)
+
+
+
     }
 }
